330e2bfaaa62e70cc1a5d611f076e59680e6fb62
Jordan <JordanChell@vivaldi.net>
2019-02-20 08:41:45 +0000

    Added gem interaction system

---
+++ unity_project/Assets/Src/GemCollection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemCollection : MonoBehaviour
+{
+    public Rigidbody2D RigidBody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RigidBody2D = this.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Gem")
+        {
+            collision.gameObject.SetActive(false);
+        }
+    }
+}
