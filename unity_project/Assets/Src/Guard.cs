78b5af8bffd57d63861a3b623dbadce7c024d19f
Jordan <JordanChell@vivaldi.net>
2019-02-27 10:36:06 +0000

    Added working guard prefab with script

--- unity_project/Assets/Src/Guard.cs
+++ unity_project/Assets/Src/Guard.cs
@@ -2,26 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelExit : MonoBehaviour
-{ 
+public class Guard : MonoBehaviour
+{
+    public float Stationary;
+    private float SleepTime;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SleepTime = Time.time + Stationary;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time > SleepTime)
+        {
+            transform.Rotate(0, 0, 90);
+            SleepTime = Time.time + Stationary;
+        }
     }
 
-    void OnCollisionEnter2D(Collision2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
-            Debug.Log("level complete!");
             other.gameObject.SetActive(false);
+            Debug.Log("FAIL");
         }
     }
 }
