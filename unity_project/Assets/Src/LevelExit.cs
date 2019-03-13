cf565ee288146505e86f0174e1ee2f92574c3a89
Jordan <JordanChell@vivaldi.net>
2019-03-13 09:26:10 +0000

    LevelExit script now adjusts level score in relation to par

--- unity_project/Assets/Src/LevelExit.cs
+++ unity_project/Assets/Src/LevelExit.cs
@@ -6,16 +6,13 @@ public class LevelExit : MonoBehaviour
 {
 
     public int par;
-    public float newScore;
     public float levelScore;
-    private float multipler;
     private int movesOver;
     private int total;
 
     // Start is called before the first frame update
     void Start()
     {
-        multipler = 0.2f;
     }
 
     // Update is called once per frame
@@ -29,19 +26,14 @@ public class LevelExit : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             Debug.Log("level complete!");
-            other.gameObject.SetActive(false);
             total = other.gameObject.GetComponent<Ball_movement>().moves;
-            newScore = other.gameObject.GetComponent<Ball_movement>().score;
-            Debug.Log(total);
             if (total > par)
             {
                 movesOver = total - par;
-                levelScore = levelScore / (1 +(multipler * movesOver));
+                levelScore = levelScore / (1 + (0.2f * movesOver));
             }
-            else if (total < par || total == par)
-            {
-                newScore += levelScore;
-            }
+            other.gameObject.GetComponent<Ball_movement>().score += levelScore;
+            other.gameObject.SetActive(false);
         }
     }
 }
