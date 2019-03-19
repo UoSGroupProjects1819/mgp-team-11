4a3dcbae03a2f94fb821a24520dc35ede8312582
Jordan <S193387@UOS.AC.UK>
2019-03-19 18:10:00 +0000

    Updated camera to always follow the player

--- unity_project/Assets/Src/CameraControls.cs
+++ unity_project/Assets/Src/CameraControls.cs
@@ -6,16 +6,26 @@ public class CameraControls : MonoBehaviour
 {
     public Camera Camera;
     public float CameraSize;
+    public GameObject player;
+    private float playerX;
+    private float playerY;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerX = player.transform.position.x;
+        playerY = player.transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (player.activeSelf == true)
+        {
+            playerX = player.transform.position.x;
+            playerY = player.transform.position.y;
+            this.transform.position = new Vector3(playerX, playerY, -10);
+        }
         
         if (Input.GetKey("c"))
         {
