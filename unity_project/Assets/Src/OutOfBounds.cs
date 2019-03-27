20b0c8e52637b0945a7ca9b0d703d29659a3f5f7
Jordan <JordanChell@Vivaldi.net>
2019-03-27 10:24:54 +0000

    Added OOB script to deal with bug where player can shoot out of level

---
+++ unity_project/Assets/Src/OutOfBounds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.transform.position = new Vector2(collision.gameObject.GetComponent<BallMovement>().Spawn.gameObject.transform.position.x, collision.gameObject.GetComponent<BallMovement>().Spawn.transform.position.y);
+        }
+    }
+}
